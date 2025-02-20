3a2f9cd611f3a74c4a852bd90c6c930cdf85db85
YuraFedorets <52930288+YuraFedorets@users.noreply.github.com>
2025-02-20 22:24:37 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,39 +2,40 @@ import { createServer } from 'node:http';
 import { createWriteStream } from 'node:fs';
 import { readFile } from 'node:fs'; 
 import { URL } from 'url';
+
 const logStream = createWriteStream('log.txt', { flags: 'a' });
 const server = createServer((req, res) => {
   const reqUrl = new URL(req.url, 'http://localhost');
 
   switch (reqUrl.pathname) {
     case '/': {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Selling banana, humans!\n');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1 style="font-size: 24px; font-weight: bold;">Selling banana, humans!</h1>');
         break;
     }
     
     case '/user': {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Joking not bananas\n');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1 style="font-size: 24px; font-weight: bold;">Joking not bananas</h1>');
         break;
     }
     
     case '/favicon.ico': {
         readFile("C:\\Users\\Fedor\\.vscode\\cli\\favicon.ico", (err, data) => {
             if (err) {
-                res.writeHead(500, { 'Content-Type': 'favicon.ico' });
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Error loading favicon.ico\n');
                 return;
             }
-            res.writeHead(200, { 'Content-Type': 'favicon.ico' });
+            res.writeHead(200, { 'Content-Type': 'image/x-icon' });
             res.end(data);
         });
         break;
     }
     
     default: {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 not found\n');
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1 style="font-size: 24px; font-weight: bold;">404 not found</h1>');
         break;
     }
   }
